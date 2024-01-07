1e5c23f70e129446e1e9f153865cbab48ef1d296
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2024-01-07 18:01:39 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt(); // 시험 점수 입력 받기
+
+        switch (score / 10) {
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
+    }
+}
