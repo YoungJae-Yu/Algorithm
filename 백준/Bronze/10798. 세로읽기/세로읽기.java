1d1d66b9cdc9059f282d195fcaf7d010038daadb
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2024-02-18 21:17:58 +0900

    [Bronze I] Title: 세로읽기, Time: 200 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[5];
+        int maxLen = 0;
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.next();
+            maxLen = Math.max(maxLen, arr[i].length());
+        }
+
+        for (int i = 0; i < maxLen; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i < arr[j].length())
+                    System.out.print(arr[j].charAt(i));
+            }
+        }
+    }
+}
