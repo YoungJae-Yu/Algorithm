fce6deec2aa566be7ff09b6cb40cc78fbd83b914
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2024-05-04 23:49:07 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 220 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        long result = factorial(N);
+        
+        System.out.println(result);
+    }
+    
+    public static long factorial(int N) {
+        if (N <= 1) {
+            return 1;
+        } else {
+            return N * factorial(N - 1);
+        }
+    }
+}
