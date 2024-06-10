b814635e0f99068244601f797214d823f5b024b5
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2024-06-10 22:41:53 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 928 ms, Memory: 124976 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 표의 크기
+        int M = Integer.parseInt(st.nextToken()); // 합을 구해야 하는 횟수
+
+        int[][] table = new int[N + 1][N + 1]; // 테이블 (1-index)
+        int[][] dp = new int[N + 1][N + 1]; // 누적 합 DP 테이블
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                table[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + table[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+            bw.write(result + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
