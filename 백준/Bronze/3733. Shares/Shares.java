b142021a8ab7878aa0f6df0b4d9634deee94507f
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2024-12-09 11:17:56 +0900

    [Bronze V] Title: Shares, Time: 180 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int N = scanner.nextInt();
+            int S = scanner.nextInt();
+            System.out.println(S / (N + 1));
+        }
+        scanner.close();
+    }
+}
