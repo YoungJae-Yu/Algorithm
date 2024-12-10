0c2f447e96e2e179d828bea05e808efcec562a9e
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2024-12-10 13:59:21 +0900

    [Bronze V] Title: SciComLove, Time: 172 ms, Memory: 17536 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력 받기
+        int N = sc.nextInt();
+        
+        // SciComLove 출력
+        for (int i = 0; i < N; i++) {
+            System.out.println("SciComLove");
+        }
+        
+        sc.close();
+    }
+}
