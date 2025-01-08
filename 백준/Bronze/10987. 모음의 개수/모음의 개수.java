cc1622d522fa8e5396a17be9b2ed1111a59f1841
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2025-01-08 17:33:15 +0900

    [Bronze IV] Title: 모음의 개수, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if ("aeiou".indexOf(c) != -1) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
