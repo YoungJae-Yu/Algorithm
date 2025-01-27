3ba51915652511b893f869853931dbddbdf137fd
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2025-01-28 02:28:03 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        System.out.println(Math.min(n / 2, m / 2));
+        scanner.close();
+    }
+}
