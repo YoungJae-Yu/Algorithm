53f1c93b48498aeab6d7574d9f00d0ccef7eb1d4
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2025-05-25 22:21:21 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 344 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+        int[] a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                    k--;
+                    if (k == 0) {
+                        StringBuilder sb = new StringBuilder();
+                        for (int x : a) {
+                            sb.append(x).append(' ');
+                        }
+                        System.out.print(sb.toString().trim());
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.print(-1);
+    }
+}
