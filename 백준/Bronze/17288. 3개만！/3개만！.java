2f295af8a9772079a2b788190febae07a83de879
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2025-07-25 10:51:46 +0900

    [Bronze I] Title: 3개만!, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/17288. 3개만！/3개만！.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int cnt = 1, ans = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) - s.charAt(i - 1) == 1) {
+                cnt++;
+            } else {
+                if (cnt == 3) {
+                    ans++;
+                }
+                cnt = 1;
+            }
+        }
+        if (cnt == 3) {
+            ans++;
+        }
+        System.out.print(ans);
+    }
+}
