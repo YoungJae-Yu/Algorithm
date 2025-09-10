63f178b3d9b3adad1a7a88d51ade71182c84f1f8
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2025-09-10 16:19:10 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] price = new int[n + 1];
+        for (int i = 1; i <= n; i++) price[i] = Integer.parseInt(st.nextToken());
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + price[j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
