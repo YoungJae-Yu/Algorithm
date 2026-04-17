2c19fdc7b5b0dc7c15fdfd9e393b325a86292323
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2026-04-17 12:14:21 +0900

    [2026-04-17] LeetCode #937 Online Stock Span (Medium)

---
+++ Medium/937.Online Stock Span/Solution.java
@@ -0,0 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class StockSpanner {
+    
+    private final Deque<int[]> stack;
+    
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        
+        stack.push(new int[]{price, span});
+        
+        return span;
+    }
+}
