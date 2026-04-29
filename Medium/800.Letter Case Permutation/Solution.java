4bae2629dd5e93f10516d5fc56d72d3fa155a43c
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2026-04-29 12:59:27 +0900

    [2026-04-29] LeetCode #800 Letter Case Permutation (Medium)

---
+++ Medium/800.Letter Case Permutation/Solution.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> result = new ArrayList<>();
+        backtrack(s.toCharArray(), 0, result);
+        return result;
+    }
+
+    private void backtrack(char[] chars, int index, List<String> result) {
+        if (index == chars.length) {
+            result.add(new String(chars));
+            return;
+        }
+
+        backtrack(chars, index + 1, result);
+
+        if (Character.isLetter(chars[index])) {
+            chars[index] ^= 32;
+            backtrack(chars, index + 1, result);
+            chars[index] ^= 32;
+        }
+    }
+}
