c6382e157334f4575dba4e07fcdbd6ec5b68288a
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2026-05-13 12:59:15 +0900

    [2026-05-13] LeetCode #1063 Best Sightseeing Pair (Medium)

---
+++ Medium/1063.Best Sightseeing Pair/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int maxScore = 0;
+        int maxLeft = values[0];
+        
+        for (int j = 1; j < values.length; j++) {
+            maxScore = Math.max(maxScore, maxLeft + values[j] - j);
+            maxLeft = Math.max(maxLeft, values[j] + j);
+        }
+        
+        return maxScore;
+    }
+}
