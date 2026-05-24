85fa3df5941471cc613449cfcab8974e504f002a
유영재 <130721730+YoungJae-Yu@users.noreply.github.com>
2026-05-24 12:53:19 +0900

    [2026-05-24] LeetCode #1050 Construct Binary Search Tree from Preorder Traversal (Medium)

--- Medium/1050.Construct Binary Search Tree from Preorder Traversal/Solution.java
+++ Medium/1050.Construct Binary Search Tree from Preorder Traversal/Solution.java
@@ -2,21 +2,16 @@ class Solution {
     private int index = 0;
 
     public TreeNode bstFromPreorder(int[] preorder) {
-        return build(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return build(preorder, Integer.MAX_VALUE);
     }
 
-    private TreeNode build(int[] preorder, int min, int max) {
-        if (index == preorder.length) return null;
-        
-        int val = preorder[index];
-        if (val < min || val > max) return null;
-        
-        TreeNode node = new TreeNode(val);
-        index++;
-        
-        node.left = build(preorder, min, val - 1);
-        node.right = build(preorder, val + 1, max);
-        
+    private TreeNode build(int[] preorder, int bound) {
+        if (index == preorder.length || preorder[index] > bound) {
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[index++]);
+        node.left = build(preorder, node.val);
+        node.right = build(preorder, bound);
         return node;
     }
 }
