2835a31bd010cbd1a308c57f9e5d90eef9781d0c
yu youngjae <130721730+YoungJae-Yu@users.noreply.github.com>
2026-09-07 12:58:16 +0900

    [2026-09-07] LeetCode #1511 Count Number of Teams (Medium)

---
+++ Medium/1511.Count Number of Teams/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            int lessLeft = 0, greaterLeft = 0, lessRight = 0, greaterRight = 0;
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) lessLeft++;
+                else if (rating[i] > rating[j]) greaterLeft++;
+            }
+            for (int k = j + 1; k < n; k++) {
+                if (rating[k] < rating[j]) lessRight++;
+                else if (rating[k] > rating[j]) greaterRight++;
+            }
+            count += lessLeft * greaterRight + greaterLeft * lessRight;
+        }
+        return count;
+    }
+}
