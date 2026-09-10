6caa4feb22fdade76e1849b7ba15a307f8e6ca1c
yu youngjae <130721730+YoungJae-Yu@users.noreply.github.com>
2026-09-10 12:11:06 +0900

    [2026-09-10] LeetCode #187 Repeated DNA Sequences (Medium)

---
+++ Medium/187.Repeated DNA Sequences/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        if (s.length() <= 10) return result;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0); map.put('C', 1); map.put('G', 2); map.put('T', 3);
+        int n = s.length();
+        int mask = (1 << 20) - 1;
+        int hash = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            hash = ((hash << 2) & mask) | map.get(s.charAt(i));
+            if (i >= 9) {
+                int c = count.getOrDefault(hash, 0);
+                if (c == 1) result.add(s.substring(i - 9, i + 1));
+                count.put(hash, c + 1);
+            }
+        }
+        return result;
+    }
+}
