456db0cba103852c40d01295b1d028df05289573
yu youngjae <130721730+YoungJae-Yu@users.noreply.github.com>
2026-09-19 12:38:36 +0900

    [2026-09-19] LeetCode #436 Find Right Interval (Medium)

--- Medium/436.Find Right Interval/Solution.java
+++ Medium/436.Find Right Interval/Solution.java
@@ -1,21 +1,27 @@
-import java.util.TreeMap;
-
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n = intervals.length;
+        int[][] starts = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            starts[i][0] = intervals[i][0];
+            starts[i][1] = i;
+        }
+        java.util.Arrays.sort(starts, (a, b) -> a[0] - b[0]);
         int[] result = new int[n];
-        
-        TreeMap<Integer, Integer> startToIndex = new TreeMap<>();
-        
         for (int i = 0; i < n; i++) {
-            startToIndex.put(intervals[i][0], i);
+            int target = intervals[i][1];
+            int lo = 0, hi = n - 1, ans = -1;
+            while (lo <= hi) {
+                int mid = (lo + hi) / 2;
+                if (starts[mid][0] >= target) {
+                    ans = starts[mid][1];
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            result[i] = ans;
         }
-        
-        for (int i = 0; i < n; i++) {
-            Integer key = startToIndex.ceilingKey(intervals[i][1]);
-            result[i] = (key == null) ? -1 : startToIndex.get(key);
-        }
-        
         return result;
     }
 }
